c88a15ef502ff05e45740aeff30480c767dd15bb
Alisson Sanches <105376629+AliSanches@users.noreply.github.com>
2024-09-09 20:57:34 -0300

    fix - InicioTelaLogin - frontend

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,3 +1,13 @@
-export default function Label => {
+export default function Label()  {
 
+    const estilo = {
+        fontSize: '16px',
+        color: '#000',
+    }
+
+    return (
+        <>
+            <label style={estilo}></label>
+        </>
+    );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,15 @@
 import "./Login.css";
 import { Box } from "@chakra-ui/react";
+import Label from "../../components/Label";
 
 export default function Login() {
   return (
     <div className="login">
-      <Box></Box>
+      <Box w={500} h={800} bg={"transparent"} boxShadow='dark-lg'  rounded='md'>
+        <Box w={500} h={200} className="imgHeaderLogin" roundedTop='md' >
+          <Label/>
+        </Box>
+      </Box>
     </div>
   );
 }
